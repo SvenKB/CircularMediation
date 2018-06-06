ec872b066a851580b276f93c44ec7389ee21d174
Sven Kleine Bardenhorst <s.bardenhrst@me.com>
2018-06-06 11:33:17 +0200

    .

--- circ_mediation_functions.R
+++ circ_mediation_functions.R
@@ -5,38 +5,17 @@ library(coda)
 library(brms)
 library(rstanarm)
 
-#Simulate data
-n <- 100
-a <- .5
-b <- tan(.5)
-c <- tan(.5)
-
-
-
-x <- rnorm(n,0,1)
-m <- rnorm(n,(a*x),1)
-y <- rep(0,n)
-
-beta <- c(c,b)
-pred <- cbind(x,m)
-linkfun   = function(x) 2 * atan(x)
-
-con <- linkfun(apply(pred, 1, "%*%", beta))
-
-y_pred <- 1+con 
-err <- rvmc(n,0,5)
-y <- y_pred + err
-y <- as.circular(y)
-
-
-plot(as.circular(y))
 
 
 # Difference in coefficients
 
-CircMed_Diff <- function(x,m,y) {
+CircMed_Diff <- function(dat) {
   
   # Standardize predictors
+  x <- dat[,1]
+  m <- dat[,2]
+  y <- dat[,3]
+  
   x <- (x-mean(x))/sqrt(var(x))
   m <- (m-mean(m))/sqrt(var(m))
   
@@ -48,7 +27,7 @@ CircMed_Diff <- function(x,m,y) {
   # Models
   mediated_model <- circular:::lm.circular.cl(y = outcome ,x = predictors , init = c(0,0))
   direct_model <- circular:::lm.circular.cl(y = outcome,x = predictors[,1], init = 0)
-  
+ 
   # Coefficients
   c <- mediated_model$coefficients[[1]]
   c_tilde <- direct_model$coefficients
@@ -58,21 +37,22 @@ CircMed_Diff <- function(x,m,y) {
   total_effect <- c_tilde
   indirect_effect <- c_tilde - c
   
-  # Effect size
-  rad2deg <- function(rad) {(rad * 180) / (pi)}
-  delta <- rad2deg(total_effect) - rad2deg(direct_effect)
-  
   # Prepare output
-  output <- list(total_effect,direct_effect,indirect_effect,delta)
-  names(output) <- c("Total","Direct","Indirect","Effect size in degrees")
+  kappa <- mediated_model$kappa
+  output <- list(total_effect,direct_effect,indirect_effect,kappa)
+  names(output) <- c("Total","Direct","Indirect","Residual Kappa")
   
   return(output)
   }
 
 
-CircMed_Product <- function(x,m,y) {
+CircMed_Product <- function(dat) {
   
   # Standardize predictors
+  x <- dat[,1]
+  m <- dat[,2]
+  y <- dat[,3]
+  
   x <- (x-mean(x))/sqrt(var(x))
   m <- (m-mean(m))/sqrt(var(m))
   
@@ -98,12 +78,10 @@ CircMed_Product <- function(x,m,y) {
   direct_effect <- c
   total_effect <- c_tilde
   
-  # Effect size
-  
-  
   # Prepare output
-  output <- list(total_effect,direct_effect,indirect_effect)
-  names(output) <- c("Total","Direct","Indirect")
+  kappa <- mediated_model$kappa
+  output <- list(total_effect,direct_effect,indirect_effect,a,b,kappa)
+  names(output) <- c("Total","Direct","Indirect","a","b","Residual kappa")
   
   return(output)
   
@@ -112,9 +90,13 @@ CircMed_Product <- function(x,m,y) {
 
 
 
-CircMed_Reparameter <- function(x,m,y) {
+CircMed_Reparameter <- function(dat) {
   
   # Standardize predictors
+  x <- dat[,1]
+  m <- dat[,2]
+  y <- dat[,3]
+  
   x <- (x-mean(x))/sqrt(var(x))
   m <- (m-mean(m))/sqrt(var(m))
   
@@ -140,17 +122,22 @@ CircMed_Reparameter <- function(x,m,y) {
   direct_effect <- total_effect-indirect_effect
   
   # Prepare output
-  output <- list(total_effect,direct_effect,indirect_effect)
-  names(output) <- c("Total","Direct","Indirect")
+  kappa <- mediated_model$kappa
+  output <- list(total_effect,direct_effect,indirect_effect,kappa)
+  names(output) <- c("Total","Direct","Indirect","kappa")
   return(output)
 }
 
 
 
 
-CircMed_Bayes_Diff <- function(x,m,y) {
+CircMed_Bayes_Diff <- function(dat) {
  
   # Standardize predictors
+  x <- dat[,1]
+  m <- dat[,2]
+  y <- dat[,3]
+  
   x <- (x-mean(x))/sqrt(var(x))
   m <- (m-mean(m))/sqrt(var(m))
   
@@ -173,14 +160,16 @@ CircMed_Bayes_Diff <- function(x,m,y) {
   mcmcsum <- summary(mcmc(mediation_sample_difference)) 
   
   # Combine into a table. 
-  cbind(mcmcsum$statistics, mcmcsum$quantiles)
+  list(cbind(mcmcsum$statistics, mcmcsum$quantiles),kappa=mediated_model$kp_mean)
 }
 
-
-
-CircMed_Bayes_Product <- function (x,m,y) {
+CircMed_Bayes_Product <- function (dat) {
   
   # Standardize predictors
+  x <- dat[,1]
+  m <- dat[,2]
+  y <- dat[,3]
+  
   x <- (x-mean(x))/sqrt(var(x))
   m <- (m-mean(m))/sqrt(var(m))
   
@@ -193,37 +182,41 @@ CircMed_Bayes_Product <- function (x,m,y) {
   
   # Predictor-Mediator model
   #pred_med <- brm(m~x, data=data, warmup = 1000, iter = 51000, chains = 1)
-  pred_med <- stan_lm(m~x, data=data, prior = NULL, chains = 2, iter = 50000, algorithm = "sampling")
-  
+  pred_med <- lm(m~x, data=data)
+  a <- rnorm(50000,pred_med$coefficients[2], summary(pred_med)$coefficients[2,2])
   # Mediated model
   mediated_model  <- circGLM(y ~ x+m, data = data, Q = 50000)
   
   # Total model
   total_model <- circGLM(y ~ x, data = data, Q = 50000)
-
+  
   mediation_sample_product <- cbind(total    = total_model$bt_chain[,1], 
-                                       direct   = mediated_model$bt_chain[,1],
-                                       indirect = mediated_model$bt_chain[,2]*posterior_samples(pred_med, pars = "x"))
-
+                                    direct   = mediated_model$bt_chain[,1],
+                                    indirect = mediated_model$bt_chain[,2]*a,
+                                    a = a,
+                                    b = mediated_model$bt_chain[,2])
+  
   # Obtain a summary of effects
   mcmcsum <- summary(mcmc(mediation_sample_product)) 
   
   # Combine into a table. 
-  cbind(mcmcsum$statistics, mcmcsum$quantiles)
-
-  }
-
-
-sim_data <- function(a,b,c,n) {
+  list(cbind(mcmcsum$statistics, mcmcsum$quantiles),kappa=mediated_model$kp_mean)
   
+}
+
+
+sim_data <- function(a,b,c,n,N) {
+ 
+  data <- vector("list",N)
+  linkfun   = function(x) 2 * atan(x)
+  for (i in 1:N) {
   x <- rnorm(n,0,1)
   m <- rnorm(n,(a*x),1)
   y <- rep(0,n)
-  
+
   beta <- c(c,b)
   pred <- cbind(x,m)
-  linkfun   = function(x) 2 * atan(x)
-  
+ 
   con <- linkfun(apply(pred, 1, "%*%", beta))
   
   y_pred <- 1+con 
@@ -231,11 +224,10 @@ sim_data <- function(a,b,c,n) {
   y <- y_pred + err
   y <- as.circular(y)
   
-  data.frame(x,m,y)
+  data[[i]] <- data.frame(x,m,y)
+  }
+  return(data)
 }
-
-
-
 
 ###################################################################################################
 ###################################################################################################
